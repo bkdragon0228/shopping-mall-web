15365de5e35af86ffc1757f71ba4877b03423533
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-12 22:04:40 +0900

    추가 : 이미지 슬라이드

--- shopping/src/component/ImgSlicer.js
+++ shopping/src/component/ImgSlicer.js
@@ -3,10 +3,26 @@ import styles from './ImgSlicer.module.css';
 
 const ImgSlicer = () => {
     return (
-        <div className={styles.mainImg}>
-            <a>
-                <img src="images/bigimg.png"></img>
-            </a>
+        <div className={styles.slider}>
+            <input type="radio" name="slider" id="slide1" checked />
+            <input type="radio" name="slider" id="slide2" />
+            <input type="radio" name="slider" id="slide3" />
+            <ul className={styles.imagebox}>
+                <li>
+                    <img src="images/bigimg.png"></img>
+                </li>
+                <li>
+                    <img src="images/bigimg.png"></img>
+                </li>
+                <li>
+                    <img src="images/bigimg.png"></img>
+                </li>
+            </ul>
+            <div className={styles.bullets}>
+                <label for="slide1">&nbsp;</label>
+                <label for="slide2">&nbsp;</label>
+                <label for="slide3">&nbsp;</label>
+            </div>
         </div>
     );
 };
