4c72c769eb3b4c11d1e3c813964039d957e627f1
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-25 21:26:09 +0900

    추가 : 몽고디비 연결

---
+++ server/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI:
+        'mongodb+srv://bkdragon:kbk003178@shopping.5cw7f.mongodb.net/?retryWrites=true&w=majority',
+};
--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const mongoose = require('mongoose');
+const config = require('./config/dev');
+
+mongoose
+    .connect(config.mongoURI, {})
+    .then(() => console.log('DB connection'))
+    .catch((err) => console.error(err));
+
 app.get('/', (req, res) => {
     res.send('hello server');
 });
